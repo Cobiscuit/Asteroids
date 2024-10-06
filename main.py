ccc228d31b567f1872799fd5c13abd88cc288579
Cobiscuit <cobainirwin@gmail.com>
2024-10-06 16:42:10 -0500

    Added constants, a way for the player to be displayed and how to rotate left and right! Taking a break for a bit.

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ Update the game world
 Draw the game to the screen
 """
 import pygame
-
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     print("Starting asteroids!")
     print(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -26,7 +30,12 @@ def main():
                 return
 
         screen.fill("Black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    
 
 if __name__ == "__main__":
     main()
