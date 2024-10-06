32059cb9fb04e6d8e0670b08957d379fd353ae26
Cobiscuit <cobainirwin@gmail.com>
2024-10-06 15:04:23 -0500

    Added pygame and a few other things and began the source loop of the game, aswell as getting the screen to work and display a solid black color.

---
+++ main.py
@@ -0,0 +1,32 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+"""
+Check for player inputs
+Update the game world
+Draw the game to the screen
+"""
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting asteroids!")
+    print(SCREEN_WIDTH, SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("Black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
